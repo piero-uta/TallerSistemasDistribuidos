c60302812c5fb5617f91e159fc67235d5824b3a4
Piero <piero.suarez.zuniga@alumnos.uta.cl>
2022-12-04 20:13:50 -0300

    version 1.0
    tiene muchos errores pero funciona

--- cliente.py
+++ cliente.py
@@ -1,11 +1,20 @@
 import socket
 import pickle
+import pygame 
 import sys
-import select
+import time
+
+pygame.init()
+pygame.display.set_caption("El gato!!")
+ 
 
 HEADERSIZE = 10
-
-
+conectado = False
+s =socket.socket()
+empezarJuego=False
+board = []
+FONT = pygame.font.Font("assets/Roboto-Regular.ttf", 32)
+COLOR_TEXTO = (6, 26, 64)
 
 
 #funcion recibir mensajes
@@ -46,12 +55,98 @@ def refrescar_lobbies():
         print(i+1, lobbies[i],"/", 2)
     s.close()
 
+def verificar_ganador():
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != "":
+            ganador = board[i][0]
+            #actualizar graphical_board
+            for j in range(3):
+                graphical_board[i][j][0] = pygame.image.load(f"assets/Winning {ganador}.png")
+                        
 
-conectado = False
-s =socket.socket()
-empezarJuego=False
+            return ganador
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != "":
+            ganador = board[0][i]
+            #actualizar graphical_board
+            for j in range(3):
+                graphical_board[j][i][0] = pygame.image.load(f"assets/Winning {ganador}.png")
+            return ganador 
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != "":
+        ganador = board[0][0]
+        #actualizar graphical_board
+        for i in range(3):
+            graphical_board[i][i][0] = pygame.image.load(f"assets/Winning {ganador}.png")
+        return ganador
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != "":
+        ganador = board[0][2]
+        #actualizar graphical_board
+        for i in range(3):
+            graphical_board[i][2-i][0] = pygame.image.load(f"assets/Winning {ganador}.png")
+        return ganador
+    return ""
+    
+def crear_board():
+    for i in range(3):
+        board.append([])
+        for j in range(3):
+            board[i].append("")
+   
+def reiniciar_board():
+    for i in range(3):
+        for j in range(3):
+            board[i][j] = ""
+
+crear_board()
+
+WIDTH, HEIGHT = 900, 900
+ 
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic Tac Toe!")
+
+BOARD = pygame.image.load("assets/Board.png")
+X_IMG = pygame.image.load("assets/X.png")
+O_IMG = pygame.image.load("assets/O.png")
+
+BG_COLOR = (214, 201, 227)
+
+
+graphical_board = [[[None, None], [None, None], [None, None]], 
+                    [[None, None], [None, None], [None, None]], 
+                    [[None, None], [None, None], [None, None]]]
+
+SCREEN.fill(BG_COLOR)
+SCREEN.blit(BOARD, (64, 64))
+
+pygame.display.update()
+
+def render_board(board, ximg, oimg):
+    global graphical_board
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == "X":
+                # Create an X image and rect
+                graphical_board[i][j][0] = ximg
+                graphical_board[i][j][1] = ximg.get_rect(center=(j*300+150, i*300+150))
+            elif board[i][j] == "O":
+                graphical_board[i][j][0] = oimg
+                graphical_board[i][j][1] = oimg.get_rect(center=(j*300+150, i*300+150))
+    for i in range(3):
+        for j in range(3):
+            if graphical_board[i][j][0] is not None:
+                SCREEN.blit(graphical_board[i][j][0], graphical_board[i][j][1])
+
+def registrar_click():
+    current_pos = pygame.mouse.get_pos()
+    converted_x = (current_pos[0]-65)/835*2
+    converted_y = current_pos[1]/835*2
+    return [converted_x, converted_y]
+
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
     if conectado == False:
         opcion = input("Elije opcion: (-1 salir, 0 refrescar, 1 conectar a lobby): ")
         if opcion == "-1":
@@ -88,7 +183,34 @@ while True:
             if recibir == "empezar":
                     print("Comienza juego")
                     empezarJuego=True
-        else:
+        else:          
+            #verificar si alguien gano
+            ganador = verificar_ganador()
+            if ganador != "":
+                reiniciar_board()
+                #reiniciar graphical_board
+                for i in range(3):
+                    for j in range(3):
+                        graphical_board[i][j][0] = None
+                        graphical_board[i][j][1] = None
+
+                
+                time.sleep(2)
+                print("gano", ganador)
+                if ganador == "X":
+                    ganadorText = FONT.render("Ganaste!!", True, COLOR_TEXTO, BG_COLOR)
+                else:
+                    ganadorText = FONT.render("Perdiste!!", True, COLOR_TEXTO, BG_COLOR)
+                textRect = ganadorText.get_rect()
+                textRect.center = (WIDTH // 2, HEIGHT//2)
+                SCREEN.blit(ganadorText, textRect)
+                #esperar 3 segundos
+                pygame.display.update()
+                time.sleep(3)
+                SCREEN.fill(BG_COLOR)
+                SCREEN.blit(BOARD, (64, 64))
+                pygame.display.update()
+
             try:
                 recibir = enviar_mensaje({"opcion": "jugando"}, s)
                 
@@ -99,9 +221,26 @@ while True:
 
             if recibir == "turno":
                 print("Es tu turno")
-                movimiento = input("Elije movimiento: ")
+                ingresandoInput = True
+                while ingresandoInput:
+                    for event in pygame.event.get():
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            pos = registrar_click()
+                            pos[0]=round(pos[0])
+                            pos[1]=round(pos[1])
+                            if pos[0] < 0 or pos[0] > 2 or pos[1] < 0 or pos[1] > 2:
+                                print("Posicion invalida")
+                                continue
+                            if board[int(pos[1])][int(pos[0])] != "":
+                                print("Posicion ocupada")
+                                continue                            
+                            board[int(pos[1])][int(pos[0])] = "X"
+                            render_board(board, X_IMG, O_IMG)
+                            pygame.display.update()
+                            ingresandoInput = False
+                            break
                 try:
-                    recibir = enviar_mensaje({"opcion":"movimiento","movimiento":movimiento}, s)
+                    recibir = enviar_mensaje({"opcion":"movimiento","movimiento":pos}, s)
                 except:
                     print("error en enviar movimiento")
                     conectado = False
@@ -116,7 +255,14 @@ while True:
                     conectado = False
                     empezarJuego=True
                     s.close()
-                print("El otro jugador hizo", recibir["movimiento"])
+                print(recibir)
+                #actualizar board con x e y
+                x = int(recibir["movimiento"][1])
+                y = int(recibir["movimiento"][0])
+                board[x][y] = "O"
+                render_board(board, X_IMG, O_IMG)
+                pygame.display.update()
+
             elif recibir == "ganaste":
                 print("ganaste")
                 conectado = False
@@ -129,4 +275,4 @@ while True:
                 print("empate")
                 conectado = False
                 s.close()
-
+        
